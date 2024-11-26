83e5c0537720ae5f27c521605807b941b8c084a6
Elbastianramirez <bas.ramirezb@duocuc.cl>
2024-11-26 18:46:42 -0300

    api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox: {
+    accesstoken: 'pk.eyJ1IjoicmFmYTBjMTM2IiwiYSI6ImNtM3lzYmNvMjF2MDMyaXEyZ3BwOGRuYzcifQ.7JDWERlfmrFp-EPdRhyUVg'
+  }
 };
